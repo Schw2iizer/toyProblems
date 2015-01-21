c86b9aff343abec7b47a96ddc228ea318102f5e9
Jeff Telford <jefftelford@gmail.com>
2015-01-21 09:38:29 -0700

    added toy probem

--- toyProblems.js
+++ toyProblems.js
@@ -114,7 +114,32 @@ var fact = function(x){
 }
 alert(product);
 
-OR
+// ------------------------------------------------------------------------------------------------------------------------------
+
+// Write a function called simpleSymbols that takes in a string parameter and determines if it is an acceptable sequence by either returning the string true or false. The str parameter will be composed of + and = symbols with several letters between them (ie. ++d+===+c++==a) and for the string to be true each letter must be surrounded by a + symbol. So the string to the left would be false. The string will not be empty and will have at least one letter.
+
+
+var str = "++d+===+c++=";
+
+var simpleSymbols = function(str) {
+for (var i = 0; i < str.length; i++) {
+  if (str.charAt(i) !== "+" && str.charAt(i) !== "=") {
+    if (str.charAt(i - 1) !== "+" || str.charAt(i + 1) !== "+") {
+        return false;
+    };
+  };
+};
+    return true;
+}
+
+simpleSymbols(str);
+
+
+
+
+
+
+
 
 
 
