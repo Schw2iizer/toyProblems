01c35987ee66417b7c041eb131ca065fb7433216
Jeff Telford <jefftelford@gmail.com>
2015-02-10 09:36:41 -0700

    added problem

--- toyProblems.js
+++ toyProblems.js
@@ -293,9 +293,42 @@ var flatten = function(arr){
 }
 
 // ---------------------------------------------------------------------------------------------------------------
+//create a function that takes an array of objects as an argument and a string that represents a property of an object contained in the array and returns an object whose keys are the properties and whose values are arrays of objects that match that property.
 
-http://jsfiddle.net/cahlan/2ub9u8fj/
+//e.g.
+var arr = [
+    {name: 'Joe Montana', team: '49ers'}, 
+    {name: 'Jerry Rice', team: '49ers'}, 
+    {name: 'Steve Young', team: '49ers'},
+    {name: 'Tony Romo', team: 'Cowboys'},
+    {name: 'Fred Biletnikoff', team: 'Raiders'},
+    {name: 'Dick Butkus', team: 'Bears'},
+    {name: 'Emmitt Smith', team: 'Cowboys'},
+];
+ 
 
 
+ var chip = function(arr, str){
+	var obj = {};
+	for (var i = 0; i < arr.length; i++){
+		if (!object.hasOwnProperty(arr[i][string])){
+			object[arr[i][string]] = [];
+			object[arr[i][string]].push(arr[i])
+		}
+		else {
+			object[arr[i][string]].push(arr[i])
+		}	
+	}
+	return obj;
+}
+
+    
+//should return:
+{ 
+    '49ers': [{name: 'Joe Montana', team: '49ers'}, {name: 'Jerry Rice', team: '49ers'}, {name: 'Steve Young', team: '49ers'}],
+    'Cowboys': [{name: 'Tony Romo', team: 'Cowboys'}, {name: 'Emmitt Smith', team: 'Cowboys'}],
+    'Bears': [{name: 'Dick Butkus', team: 'Bears'}],
+    'Raiders': [{name: 'Fred Biletnikoff', team: 'Raiders'}]
+}
 
 
